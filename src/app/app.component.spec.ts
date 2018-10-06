1ee3ba4e9f1acb41e36fda2b02c5fc664fd378bd
ericksoen <michael.ericksen@gmail.com>
2018-10-05 21:11:37 -0500

    feat: add initial project assets

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,5 +1,14 @@
 import { TestBed, async } from '@angular/core/testing';
-import { AppComponent } from './app.component';
+import { Component } from '@angular/core';
+@Component({
+  selector: 'app-root',
+  template: `<h1>Welcome to {{title}}!</h1>`,
+  styles: []
+})
+export class AppComponent {
+  title = 'app';
+}
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -8,6 +17,7 @@ describe('AppComponent', () => {
       ],
     }).compileComponents();
   }));
+
   it('should create the app', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
